3a5bd67b7b89fc001bf1d111232a3454037d579c
naur9n <GITHUB_HESABINDAKI_EMAIL>
2026-07-19 14:54:20 +0300

    Enable Base mainnet x402 payments

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import { facilitator } from "@coinbase/x402";
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
@@ -23,7 +24,7 @@ app.use(cors());
 app.use(express.json({ limit: "300kb" }));
 app.use(express.static("public"));
 
-const facilitatorClient = new HTTPFacilitatorClient({ url: facilitatorUrl });
+const facilitatorClient = new HTTPFacilitatorClient(facilitator);
 const resourceServer = new x402ResourceServer(facilitatorClient);
 registerExactEvmScheme(resourceServer);
 
